fbc3739e3b9bbaffeb6880efc7fb196f7fee0010
derwind <uncertainty_principle_ss@yahoo.co.jp>
2017-08-07 20:19:57 +0900

    change calculation of VertOriginY

--- ShowVmtx/ShowVmtx.glyphsReporter/Contents/Resources/ShowVmtx.py
+++ ShowVmtx/ShowVmtx.glyphsReporter/Contents/Resources/ShowVmtx.py
@@ -24,12 +24,22 @@ class ShowVmtx(ReporterPlugin):
         self.menuName = Glyphs.localize({'en': u'Vmtx'})
 
     def background(self, layer):
+        # top of body for CJK fonts
+        ascender = Glyphs.font.masters[0].ascender
+
         # NSColor.colorWithCalibratedRed_green_blue_alpha_( 0, 0, 0, 0.15 ).set()
         NSColor.redColor().colorWithAlphaComponent_(.7).set()
+        vertOriginY = ascender
+        if hasattr(layer, "vertOrigin"):
+            vertOriginY -= layer.vertOrigin()
+        vertOriginY = int(round(vertOriginY))
+
         vertWidth = UPM
         if hasattr(layer, "vertWidth") and layer.vertWidth() >= 0:
             vertWidth = layer.vertWidth()
-        bottom = layer.bounds.origin.y - layer.BSB
+        vertWidth = int(round(vertWidth))
+
+        bottom = vertOriginY - vertWidth
         rect = NSRect(
             origin = (0, bottom),
             size=(layer.width, vertWidth)
@@ -37,3 +47,8 @@ class ShowVmtx(ReporterPlugin):
         path = NSBezierPath.bezierPathWithRect_(rect)
         path.setLineWidth_(2)
         path.stroke()
+
+        pos = NSPoint(layer.width + 20, bottom + vertWidth + 20)
+        self.drawTextAtPoint("VertOriginY={}".format(vertOriginY), pos, fontColor=NSColor.brownColor())
+        pos = NSPoint(layer.width + 20, bottom + 20)
+        self.drawTextAtPoint("VertAdvanceY={}".format(vertWidth), pos, fontColor=NSColor.brownColor())
